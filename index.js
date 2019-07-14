a76c4b1f4019cb349817d96d822b7ab09f8e7d17
Sam Parsons <samparsons269@gmail.com>
2019-07-14 15:47:52 -0400

    package json

---
+++ index.js
@@ -0,0 +1,16 @@
+// body link event listeners
+const links = {
+  github: "https://github.com/sam-parsons",
+  linkedin: "https://www.linkedin.com/in/samparsons269/"
+};
+$(".icon").on("click", e => {
+  window.open(links[e.target.id], "_self");
+});
+
+// navbar expansion event listener
+const toggle = document.getElementsByClassName("toggle-button")[0];
+const navLinks = document.getElementsByClassName("links")[0];
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
